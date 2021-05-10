b05c2bbe36164b2efa38e25ce6809cd4afd3dac0
Cristi Chirtos <cristichirtos@gmail.com>
2021-05-10 12:55:17 +0300

    Add users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
  
+  def index 
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -7,6 +13,8 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -20,9 +28,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def update 
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated!'
+      redirect_to @user
+    else 
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = 'Please log in.'
+        redirect_to login_path 
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
     end
 end
